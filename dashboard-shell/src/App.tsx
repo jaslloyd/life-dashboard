6efa85557f3540f46bbcddc48e506e3618cfc8ac
Jason Lloyd <jason.lloyddev@gmail.com>
2020-06-05 09:17:44 +0100

    Sidebar with font-awesome

--- dashboard-shell/src/App.tsx
+++ dashboard-shell/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar";
+import "./index.css";
 
 const App: React.FC = () => (
-  <div>
-    <h1>Hello from React Typescriptrm</h1>
-  </div>
+  <>
+    <Navbar />
+    <main>
+      <h1>Hello from React Typescriptrm</h1>
+    </main>
+  </>
 );
 export default App;
