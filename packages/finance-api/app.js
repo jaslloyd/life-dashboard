526681d83c518ee159b165b3a8a040466a9aa24f
Jason Lloyd <jason.lloyddev@gmail.com>
2020-06-08 09:03:04 +0100

    Add .env for environment variable add versioning to api

--- packages/finance-api/app.js
+++ packages/finance-api/app.js
@@ -1,11 +1,17 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+const indexRouter = require("./routes/index");
 
-var app = express();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+console.log(process.env);
+
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -13,6 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/api/v1", indexRouter);
 
 module.exports = app;
