73f6f34a4d1e9b25625de2b2dce298fada9ef2d3
Jason Lloyd <jason.lloyddev@gmail.com>
2020-06-09 09:34:14 +0100

    Can now login with 2FA and get unformatted portfolio

--- packages/finance-api/routes/index.js
+++ packages/finance-api/routes/index.js
@@ -1,9 +1,52 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const DeGiro = require("degiro");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send("pong");
+// TODO: Use POST request
+router.get("/login/:code", async (req, res) => {
+  const code = req.params.code;
+  const degiro = DeGiro.create({
+    oneTimePassword: code,
+    debug: true,
+  });
+  try {
+    await degiro.login();
+    res.send({
+      status: "success",
+      message: "Login Successful",
+      id: degiro.session.id,
+      accountId: degiro.session.account,
+    });
+  } catch (e) {
+    res
+      .json({
+        status: "failed",
+        message: e.toString(),
+      })
+      .status(500);
+  }
+});
+
+// TODO: Use Cookies to get session id later
+router.get("/portfolio/:sid", async (req, res) => {
+  console.log(req.params.sid);
+  const degiro = DeGiro.create({
+    sessionId: req.params.sid,
+  });
+
+  try {
+    await degiro.updateConfig();
+    const portfolio = await degiro.getPortfolio();
+
+    res.json({
+      portfolio,
+    });
+  } catch (e) {
+    res.json({
+      status: "failed",
+      message: e.toString(),
+    });
+  }
 });
 
 module.exports = router;
