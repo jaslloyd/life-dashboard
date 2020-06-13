3d61c5d04d1b54fbcd7e6e2f691bcb56235623ea
Jason Lloyd <jason.lloyddev@gmail.com>
2020-06-13 19:49:54 +0100

    Linear-gradiant

--- packages/finance-dashboard/src/App.tsx
+++ packages/finance-dashboard/src/App.tsx
@@ -23,7 +23,6 @@ interface Portfolio {
 }
 
 const FinanceApp: React.FC = () => {
-  // TODO: Move all the api calls to Finance app, like Container Presentation component pattern
   const [apiResult, setApiResult] = React.useState(null);
   const [status, setStatus] = React.useState("loading");
 
@@ -77,11 +76,12 @@ const FinanceApp: React.FC = () => {
   return (
     <DashboardShell>
       <h1>Finance Application</h1>
+      <div className="Tile skeleton"></div>
       {status === "idle" && (
-        <>
+        <div className="FinanceApp2">
           <InvestTotals totals={apiResult} />
           <InvestmentTable portfolioData={apiResult} />
-        </>
+        </div>
       )}
 
       {status === "error" && <h1>An unexpected error occurred...</h1>}
@@ -170,4 +170,69 @@ const InvestTotals: React.FC<{ totals: Portfolio }> = ({ totals }) => (
     </Tile>
   </div>
 );
+const ExternalTile: React.FC = () => {
+  const [apiResult, setApiResult] = React.useState(null);
+  const [status, setStatus] = React.useState("loading");
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  // TODO: Move this out to a util function
+  const fetchData = async () => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/v1/portfolio`, {
+        credentials: "include",
+        headers: {
+          Authorization: localStorage.getItem("SESSION_ID"),
+        },
+      });
+
+      if (res.ok) {
+        const resultJSON = await res.json();
+
+        setApiResult(resultJSON);
+        setStatus("idle");
+      } else {
+        if (res.status === 401) {
+          console.log("Need to login again");
+          setStatus("showLogin");
+        }
+      }
+    } catch (e) {
+      console.error(e);
+      setStatus("error");
+    }
+  };
+
+  const handleLogin = async (code: string) => {
+    try {
+      setApiResult("loading");
+      const resp = await fetch(`http://localhost:3000/api/v1/login/${code}`);
+
+      if (resp.ok) {
+        const json = await resp.json();
+        localStorage.setItem("SESSION_ID", json.id);
+        fetchData();
+      } else {
+        console.log("Response was not valid...");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <>
+      {status === "idle" && (
+        <>
+          <InvestTotals totals={apiResult} />
+        </>
+      )}
+      {status === "error" && <h1>An unexpected error occurred...</h1>}
+
+      {status === "showLogin" && <Login onSubmit={handleLogin} />}
+    </>
+  );
+};
 export default FinanceApp;
