08c8381b3158482554aaa9cb6ff8728f651dc8a9
Jason Lloyd <jason.lloyddev@gmail.com>
2020-06-20 10:38:14 +0100

    Move portfolio logic

--- packages/finance-api/lib/degiro.js
+++ packages/finance-api/lib/degiro.js
@@ -1,26 +1,107 @@
 const DeGiro = require("degiro");
 const fetch = require("node-fetch");
 
-const login = (code) => {
-  return new Promise(async (resolve, rej) => {
-    const degiro = DeGiro.create({
-      oneTimePassword: code,
-      debug: true,
-    });
-
-    try {
-      await degiro.login();
-
-      resolve({
-        id: degiro.session.id,
-        accountId: degiro.session.account,
-      });
-    } catch (e) {
-      rej(e);
-    }
+const login = async (code) => {
+  const degiro = DeGiro.create({
+    oneTimePassword: code,
+    debug: true,
   });
+
+  try {
+    await degiro.login();
+
+    resolve({
+      id: degiro.session.id,
+      accountId: degiro.session.account,
+    });
+  } catch (e) {
+    rej(e);
+  }
+};
+
+const getPortfolio = async (sessionId) => {
+  const degiro = DeGiro.create({
+    sessionId,
+  });
+
+  await degiro.updateConfig();
+  const portfolio = await degiro.getPortfolio();
+
+  const productIds = portfolio.portfolio.map((p) => p.id);
+  const productNames = await degiro.getProductsByIds(productIds);
+
+  const takeValueFromPortfolio = (productInfo) => (valueToFind) =>
+    productInfo.find((values) => values.name === valueToFind).value;
+
+  const result = Object.keys(productNames.data).map((pid) => {
+    const personalProductInfo = portfolio.portfolio.find((p) => p.id === pid)
+      .value;
+
+    const findValue = takeValueFromPortfolio(personalProductInfo);
+
+    return {
+      id: pid,
+      tickerSymbol: productNames.data[pid].symbol,
+      name: productNames.data[pid].name,
+      productType: productNames.data[pid].productType,
+      sharesHeld: findValue("size"),
+      currentStockValue: findValue("price"),
+      stockValueBreakEvenPrice: +findValue("breakEvenPrice").toFixed(2),
+      totalPositionValue: +findValue("value").toFixed(2),
+      stockCurrency: productNames.data[pid].currency,
+      totalBreakEvenPrice: +(
+        findValue("breakEvenPrice") * findValue("size")
+      ).toFixed(2),
+    };
+  });
+
+  const rates = await getCurrencyRates();
+
+  const totalsByCurrencies = result.reduce((acc, curr) => {
+    if (!acc[curr.stockCurrency]) {
+      acc[curr.stockCurrency] = {
+        currTotal: curr.totalPositionValue,
+        breakEvenTotal: curr.totalBreakEvenPrice,
+      };
+    } else {
+      acc[curr.stockCurrency] = {
+        currTotal: (acc[curr.stockCurrency].currTotal +=
+          curr.totalPositionValue),
+        breakEvenTotal: (acc[curr.stockCurrency].breakEvenTotal +=
+          curr.totalBreakEvenPrice),
+      };
+    }
+    return acc;
+  }, {});
+
+  console.log(totalsByCurrencies);
+
+  const overallTotalInEuro = (
+    totalsByCurrencies["EUR"].currTotal +
+    totalsByCurrencies["USD"].currTotal * rates.EUR
+  ).toFixed(0);
+
+  const overBETotalInEuro = (
+    totalsByCurrencies["EUR"].breakEvenTotal +
+    totalsByCurrencies["USD"].breakEvenTotal * rates.EUR
+  ).toFixed(0);
+  console.log(overallTotalInEuro - overBETotalInEuro);
+
+  return {
+    overallTotalInEuro,
+    portfolioItems: result,
+    portfolio,
+  };
+};
+
+const getCurrencyRates = async (baseRate = "USD") => {
+  const { rates } = await (
+    await fetch(`https://api.exchangeratesapi.io/latest?base=${baseRate}`)
+  ).json();
+  return rates;
 };
 
 module.exports = {
   login,
+  getPortfolio,
 };
--- packages/finance-api/routes/index.js
+++ packages/finance-api/routes/index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const DeGiro = require("degiro");
 const fetch = require("node-fetch");
-const { login } = require("../lib/degiro");
+const { login, getPortfolio } = require("../lib/degiro");
 
 // TODO: Use POST request
 router.post("/login", async (req, res) => {
-  console.log(req.body);
   const code = req.body.code;
   if (!code) {
     return res.status(401).json({
@@ -41,80 +40,16 @@ router.get("/portfolio", async (req, res) => {
       message: "Please re-login to your investment account",
     });
   } else {
-    const degiro = DeGiro.create({
-      sessionId: req.headers.authorization,
-    });
-
     try {
-      await degiro.updateConfig();
-      const portfolio = await degiro.getPortfolio();
-
-      const productIds = portfolio.portfolio.map((p) => p.id);
-      const productNames = await degiro.getProductsByIds(productIds);
-
-      const takeValueFromPortfolio = (productInfo) => (valueToFind) =>
-        productInfo.find((values) => values.name === valueToFind).value;
-
-      const result = Object.keys(productNames.data).map((pid) => {
-        const personalProductInfo = portfolio.portfolio.find(
-          (p) => p.id === pid
-        ).value;
-
-        const findValue = takeValueFromPortfolio(personalProductInfo);
-
-        return {
-          id: pid,
-          tickerSymbol: productNames.data[pid].symbol,
-          name: productNames.data[pid].name,
-          productType: productNames.data[pid].productType,
-          sharesHeld: findValue("size"),
-          currentStockValue: findValue("price"),
-          stockValueBreakEvenPrice: +findValue("breakEvenPrice").toFixed(2),
-          totalPositionValue: +findValue("value").toFixed(2),
-          stockCurrency: productNames.data[pid].currency,
-          totalBreakEvenPrice: +(
-            findValue("breakEvenPrice") * findValue("size")
-          ).toFixed(2),
-        };
-      });
-
-      const { rates } = await (
-        await fetch("https://api.exchangeratesapi.io/latest?base=USD")
-      ).json();
-
-      const totalsByCurrencies = result.reduce((acc, curr) => {
-        if (!acc[curr.stockCurrency]) {
-          acc[curr.stockCurrency] = {
-            currTotal: curr.totalPositionValue,
-            breakEvenTotal: curr.totalBreakEvenPrice,
-          };
-        } else {
-          acc[curr.stockCurrency] = {
-            currTotal: (acc[curr.stockCurrency].currTotal +=
-              curr.totalPositionValue),
-            breakEvenTotal: (acc[curr.stockCurrency].breakEvenTotal +=
-              curr.totalBreakEvenPrice),
-          };
-        }
-        return acc;
-      }, {});
-
-      console.log(totalsByCurrencies);
-
-      const overallTotalInEuro = (
-        totalsByCurrencies["EUR"].currTotal +
-        totalsByCurrencies["USD"].currTotal * rates.EUR
-      ).toFixed(0);
-
-      const overBETotalInEuro = (
-        totalsByCurrencies["EUR"].breakEvenTotal +
-        totalsByCurrencies["USD"].breakEvenTotal * rates.EUR
-      ).toFixed(0);
-      console.log(overallTotalInEuro - overBETotalInEuro);
+      const {
+        overallTotalInEuro,
+        portfolioItems,
+        portfolio,
+      } = await getPortfolio(req.headers.authorization);
 
       res.json({
         overallTotalInEuro,
-        portfolioItems: result,
+        portfolioItems,
         portfolio,
       });
     } catch (e) {
